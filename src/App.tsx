9a9922361e8583cc05ae1a86f735392d3c96edc3
certifiedTboy <etosin70@gmail.com>
2026-06-29 23:03:43 +0100

    remove un-used files and components

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import {
   useLocation,
 } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "./components/ui/toaster";
-import { TooltipProvider } from "./components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import NotFound from "./pages/not-found";
 import { ThemeToggle } from "@/components/theme-toggle";
@@ -29,52 +27,49 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <TooltipProvider>
-          <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-            {isCustomDesignPage ? (
-              <CustomDesign />
-            ) : (
-              <AppLayout
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-                selectedCategory={selectedCategory}
-                setSelectedCategory={setSelectedCategory}
-              >
-                <div className="flex h-screen bg-background text-foreground overflow-hidden">
-                  <main className="flex-1 h-screen overflow-y-auto relative scrollbar-thin scrollbar-thumb-primary/50 scrollbar-track-background">
-                    <div className="fixed right-0 mr-12.5 mt-12.5">
-                      <ThemeToggle />
-                    </div>
-                    <Switch>
-                      <Route path="/">
-                        <Home
-                          searchQuery={searchQuery}
-                          selectedCategory={selectedCategory}
-                        />
-                      </Route>
-                      <Route
-                        path="/architecture/:id"
-                        component={ArchitectureDetail}
-                      />
-
-                      <Route path="/custom-design" component={CustomDesign} />
-                      <Route component={NotFound} />
-                    </Switch>
-                  </main>
-                  <div className="fixed bottom-8 right-8">
-                    <Link
-                      to="/custom-design"
-                      className="cursor-pointer rounded-xl bg-primary flex items-center justify-center text-primary-foreground font-semibold p-4"
-                    >
-                      Create Custom Design
-                    </Link>
+        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          {isCustomDesignPage ? (
+            <CustomDesign />
+          ) : (
+            <AppLayout
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            >
+              <div className="flex h-screen bg-background text-foreground overflow-hidden">
+                <main className="flex-1 h-screen overflow-y-auto relative scrollbar-thin scrollbar-thumb-primary/50 scrollbar-track-background">
+                  <div className="fixed right-0 mr-12.5 mt-12.5">
+                    <ThemeToggle />
                   </div>
+                  <Switch>
+                    <Route path="/">
+                      <Home
+                        searchQuery={searchQuery}
+                        selectedCategory={selectedCategory}
+                      />
+                    </Route>
+                    <Route
+                      path="/architecture/:id"
+                      component={ArchitectureDetail}
+                    />
+
+                    <Route path="/custom-design" component={CustomDesign} />
+                    <Route component={NotFound} />
+                  </Switch>
+                </main>
+                <div className="fixed bottom-8 right-8">
+                  <Link
+                    to="/custom-design"
+                    className="cursor-pointer rounded-xl bg-primary flex items-center justify-center text-primary-foreground font-semibold p-4"
+                  >
+                    Create Custom Design
+                  </Link>
                 </div>
-              </AppLayout>
-            )}
-          </WouterRouter>
-          <Toaster />
-        </TooltipProvider>
+              </div>
+            </AppLayout>
+          )}
+        </WouterRouter>
       </ThemeProvider>
     </QueryClientProvider>
   );
